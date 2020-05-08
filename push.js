202f8bee33dea479e0585a08ed29a2db3a87e87f
srikanthzen <srikanth@zenwork.com>
2020-05-08 17:20:34 +0530

    fix

--- push.js
+++ push.js
@@ -14,15 +14,8 @@ let vapidKeys = {
     privateKey: 'hNj6czKgWI1gBZoi7TIJx8hCyGDrRX75IAoiWhJv8b4'
 }
 
-push.setVapidDetails('mailto :srikanth@zenwork.com', vapidKeys.publicKey, vapidKeys.privateKey)
+push.setVapidDetails('mailto :test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {
-    "endpoint": "https://fcm.googleapis.com/fcm/send/c5f5UExHYvo:APA91bGxZqUUVEIhYNgTs1JgdA2SvBaEYZlZSHUrQJjjvea2kA9homKhz0XZbGHKURhblYJwIe2wWSBQqZlKDaVsvT9MAGZlNxRxwbD59F86EtsLrDNCOKSRAdMFa0ct7tl94pugqde0",
-    "expirationTime": null,
-    "keys": {
-        "p256dh": "BNnCABRYKcgXmkTJgwvREz07mCKxxWh2WSTWR_MqLfNbNIm3q8dSUk-oHJc-6ILBJ3l7x0W52P4mEtivKUn2r7U",
-        "auth": "mRyUN8IEiXtrDsY5qVOHZw"
-    }
-};
+let sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/c5f5UExHYvo:APA91bGxZqUUVEIhYNgTs1JgdA2SvBaEYZlZSHUrQJjjvea2kA9homKhz0XZbGHKURhblYJwIe2wWSBQqZlKDaVsvT9MAGZlNxRxwbD59F86EtsLrDNCOKSRAdMFa0ct7tl94pugqde0", "expirationTime": null, "keys": { "p256dh": "BNnCABRYKcgXmkTJgwvREz07mCKxxWh2WSTWR_MqLfNbNIm3q8dSUk-oHJc-6ILBJ3l7x0W52P4mEtivKUn2r7U", "auth": "mRyUN8IEiXtrDsY5qVOHZw" } }
 
 push.sendNotification(sub, 'test message')
